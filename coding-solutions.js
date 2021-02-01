fb1204a24c64972102e0745a65bea06b6c8e6c65
JL <jennalbouche@gmail.com>
2021-02-01 13:30:33 -0800

    add solution to bouncing ball recursion problem

--- coding-solutions.js
+++ coding-solutions.js
@@ -59,3 +59,15 @@ function squareDigits(num){
   } 
   return parseInt(squaredArr.join(''))
 }
+
+//Bouncing Balls (recursion problem)
+//https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+//Solution:
+
+function bouncingBall(h,  bounce,  window) {
+  if (h > 0 && bounce > 0 && bounce < 1 && window < h){
+    return h < window ? -1 : 2 + bouncingBall((h * bounce), bounce, window);
+  } else {
+    return -1;
+  }
+}
