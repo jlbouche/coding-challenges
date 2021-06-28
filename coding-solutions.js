acc2c9df4ec3a04cb105cdfdd434686912ac0b9b
JL <jennalbouche@gmail.com>
2021-06-28 10:52:12 -0700

    average array solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -95,3 +95,13 @@ function dirReduc(arr){
   const opposites = {"NORTH": "SOUTH", "EAST": "WEST", "SOUTH": "NORTH", "WEST": "EAST"}
   return arr.reduce((acc, val) => (opposites[acc.slice(-1)] === val ? acc.pop() : acc.push(val), acc), [])
 }
+
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+//Return the average of the given array rounded down to its nearest integer.
+
+//The array will never be empty.
+
+function getAverage(marks){
+  return Math.floor(marks.reduce((a, b) => a + b) / marks.length);
+}
