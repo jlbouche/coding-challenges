cb22a6200a83eeb4dc2974057758ef7ea1b025bd
JL <jennalbouche@gmail.com>
2021-06-29 10:59:36 -0700

    solve cat/dog/human years

--- coding-solutions.js
+++ coding-solutions.js
@@ -115,3 +115,28 @@ function getAverage(marks){
 function checkAlive(health){
   return health > 0;
 }
+
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+function findHumanCatDogYears(humanYears){
+  let catYears = 15 + (humanYears > 1 ? 9 : 0) + (humanYears > 2 ? 4*(humanYears-2) : 0);
+  let dogYears = 15 + (humanYears > 1 ? 9 : 0) + (humanYears > 2 ? 5*(humanYears-2) : 0);
+  return [humanYears, catYears, dogYears];
+}
