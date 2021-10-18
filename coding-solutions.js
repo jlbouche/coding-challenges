5d0445968251a0d6b825bfe7e6c4e18119b87dac
jlbouche <jennalbouche@gmail.com>
2021-10-18 13:25:23 -0700

    class avg comparison solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -176,3 +176,18 @@ function positiveSum(arr) {
   arr.map(x => (x > 0) ? sum = sum+x : 0)
   return sum;
 }
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let avg = 0;
+  for (let i=0; i < classPoints.length; i++){
+    avg = avg + classPoints[i] / classPoints.length
+  }
+  return avg > yourPoints ? false : true
+}
