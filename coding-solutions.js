c881a97ebd4ed3da2687e1fe710f6ad28a35a6e7
jlbouche <jennalbouche@gmail.com>
2022-01-10 09:41:44 -0800

    solution for identical elem in array

--- coding-solutions.js
+++ coding-solutions.js
@@ -283,3 +283,11 @@ function smallEnough(arr, limit){
 function smallEnough(a, limit){
   return Math.max(...a) <= limit
 }
+
+// Given two arrays of integers m and n, test if they contain at least one identical element. Return true if they do; false if not.
+
+// Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
+
+function duplicateElements(m, n) {
+  return m.some(val => n.includes(val))
+}
