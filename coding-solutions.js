b4492111e253e9a8151298caab8bc3ee9abe5d6a
jlbouche <jennalbouche@gmail.com>
2022-02-15 12:32:57 -0800

    solution to array same challenge

--- coding-solutions.js
+++ coding-solutions.js
@@ -351,3 +351,13 @@ function high(str){
 function arrayDiff(a, b) {
   return a.filter(elem => !b.includes(elem))
 }
+
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+function comp(arr1, arr2){
+  if (!arr1 || !arr2 || arr1.length !== arr2.length){
+    return false;
+  } else {
+    return arr1.reduce((prevVal, currVal) => prevVal+currVal, 1) === arr2.reduce((prevVal, currVal) => prevVal + Math.sqrt(currVal), 1)
+  }
+}
