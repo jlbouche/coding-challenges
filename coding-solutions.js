1c0f0c78d65c9bdbbd4b7635ec608b8174ba283e
jlbouche <jennalbouche@gmail.com>
2022-02-16 11:13:37 -0800

    getsum solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -390,3 +390,12 @@ function basicOp(op, val1, val2){
     : 0
 }
 
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+function getSum( a,b ){
+  let min = Math.min(a, b) 
+  let max = Math.max(a, b);
+  return a===b ? a : ((max-min)+1) * (min + max) / 2
+}
