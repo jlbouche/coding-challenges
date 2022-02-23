9b90c3db8946e2a12fd7acbe2d83d98e2296e359
jlbouche <jennalbouche@gmail.com>
2022-02-23 14:31:45 -0800

    printer error solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -462,3 +462,14 @@ function sumArray(array) {
   return (!array || array.length<=2) ? 0 :  array.sort((a,b) => a-b).slice(1,-1).reduce((a,b) => a+b)
 }
 
+//You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+function printerError(s) {
+  let count = 0;
+  for(let i = 0; i < s.length; i++) {
+    if (s[i] > "m") {
+      count++;
+    }
+  }
+  return `${count}/${s.length}`;
+}
