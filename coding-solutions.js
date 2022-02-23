7c90e9a5f32af358990fc32b992595bbf0ec2440
jlbouche <jennalbouche@gmail.com>
2022-02-23 14:48:36 -0800

    separate string by camelcase solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -473,3 +473,9 @@ function printerError(s) {
   }
   return `${count}/${s.length}`;
 }
+
+//Complete the solution so that the function will break up camel casing, using a space between words.
+
+function solution(string) {
+  return string.split(/(?=[A-Z])/).join(" ")
+}
