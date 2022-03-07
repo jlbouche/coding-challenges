fe0cd86a2ea17d0ec4621333aaf16a9936034d18
jlbouche <jennalbouche@gmail.com>
2022-03-07 14:45:02 -0800

    Fizzbuzz problem solution

--- coding-solutions.js
+++ coding-solutions.js
@@ -509,3 +509,21 @@ function getMiddle(s) {
   }
   return s.substring(position, position+length)
 }
+
+//Fizzbuzz problem
+
+var fizzBuzz = function(n) {
+  let arr = []
+  for (let i = 1; i <= n; i++) {
+     if (i%3===0 && i%5===0){
+       arr.push("FizzBuzz")
+     } else if (i%3===0){
+       arr.push("Fizz")
+     } else if (i%5===0){
+       arr.push("Buzz")
+     } else {
+       arr.push(i.toString())
+     }
+  }
+return arr;
+};
